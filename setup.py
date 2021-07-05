05d944fa6162612a77dea5897738ea09adad8a19
Thiago Hiromi <thhi@hellofresh.com>
2021-07-05 18:27:10 +0200

    Add minimal package setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='anime-quotes',
+      version='0.1',
+      description='Well known anime quotes',
+      url='http://github.com/thiromi/anime-quotes',
+      packages=['anime_quotes'],
+      scripts=['bin/anime-quote'],
+      zip_safe=True)
